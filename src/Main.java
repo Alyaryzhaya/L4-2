9c128908f23fa370991a532a213aad2727415750
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-11 22:53:29 +0300

    reformat

--- src/Main.java
+++ src/Main.java
@@ -19,17 +19,17 @@ public class Main {
         // В ДВА ЭТАПА
         CalcService service = new CalcService();  // создали ОБЪЕКТ
 
- //       int myMax = service.max(8, 78); // вызвали объект и попросили сделать ДЕЙСТВИЕ max.
-                                          // Возвращенный функцией РЕЗУЛЬТАТ положить в переменную int myMax
+        //       int myMax = service.max(8, 78); // вызвали объект и попросили сделать ДЕЙСТВИЕ max.
+                                                 // Возвращенный функцией РЕЗУЛЬТАТ положить в переменную int myMax
 
-       //        int x = 5;  // можно вызывать методом не конкретные цифры, а переменные
-//        int y = 6;
-//        int myMax = service.max(x, y);
- //       System.out.println(myMax + "!");   // можем проверить результат в переменной myMax
+        //       int x = 5;  // можно вызывать методом не конкретные цифры, а переменные
+        //       int y = 6;
+        //       int myMax = service.max(x, y);
+        //       System.out.println(myMax + "!"); // можем проверить результат в переменной myMax
 
-        System.out.println();    // чтобы было разделение между результатами на печати
-        System.out.println("7/9"); // название проверки
-        System.out.println(service.max(7,9)); // Можно сразу после создания объекта вывести на экран его действие (max)
+        System.out.println();  // чтобы было разделение между результатами на печати
+        System.out.println("7/9");  // название проверки
+        System.out.println(service.max(7, 9));  // Можно сразу после создания объекта вывести на экран его действие (max)
 
         System.out.println();
         System.out.println("10/10");
